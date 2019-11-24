fe998a9002e83c690bb153587e12f9f89e380294
Roy Hsu <roy.hsu@tinyworld.cc>
2019-11-24 15:19:18 +0800

    handle dependencies and xcode project.

---
+++ Sources/TinyPipeline/TinyPipeline.h
@@ -0,0 +1,19 @@
+//
+//  TinyPipeline.h
+//  TinyPipeline
+//
+//  Created by Roy Hsu on 2019/11/24.
+//  Copyright © 2019 TinyWorld. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TinyPipeline.
+FOUNDATION_EXPORT double TinyPipelineVersionNumber;
+
+//! Project version string for TinyPipeline.
+FOUNDATION_EXPORT const unsigned char TinyPipelineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TinyPipeline/PublicHeader.h>
+
+
