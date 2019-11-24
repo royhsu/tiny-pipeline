eed04ec72c7ec33b1cb7187a4c4ef545aaa2ceea
Roy Hsu <roy.hsu@tinyworld.cc>
2019-11-24 13:56:05 +0800

    add docs.

--- Sources/TinyPipeline/DuplexBound+ForwardCurrentResult.swift
+++ Sources/TinyPipeline/DuplexBound+ForwardCurrentResult.swift
@@ -2,6 +2,10 @@
 
 import TinyCombine
 
+/// Try to forward the current result from the given context. Only fallback to target bound when the context
+/// remains unresolved.
+/// - parameter context: the current context.
+/// - parameter fallback: the target bound for fall-backing.
 func forwardCurrentResult<Success, Failure>(
     context: DuplexBoundContext<Success, Failure>,
     fallback: @escaping Duplex<Success, Failure>.Bound
